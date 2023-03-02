bc5dcdae9b1e8633268264409b1aeccf86aaf7ce
Edwin Mambo <edwinmambo33@gmail.com>
2023-03-02 14:22:44 +0300

    feat: Shared Module Setup
    - Created shared module - handles all shared files
    - Added necessary folder
      - components
      - models - to handle interfaces required in the application
      - services - to handle communication between components
      - pipes
    - Import the shared module to public and private modules

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PublicComponent
-  ],
-  imports: [
-    CommonModule,
-    PublicRoutingModule
-  ]
+  declarations: [PublicComponent],
+  imports: [CommonModule, PublicRoutingModule, SharedModule],
 })
-export class PublicModule { }
+export class PublicModule {}
